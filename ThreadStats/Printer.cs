3601bfca6c229511518c9de19cef326236ab520b
Karatekaneen <bauhn_@hotmail.com>
2019-10-30 16:12:08 +0100

    Modify PrintNumbers() method to force the current thread to pause for a randomly generated amount of time and the Main() method to create an array of ten(uniquely named) Thread objects, each of which is making calls on the same instance of the Printer object and display the result.

--- ThreadStats/Printer.cs
+++ ThreadStats/Printer.cs
@@ -9,39 +9,19 @@ namespace ThreadStats
 {
     class Printer
     {
-        public static void StartPrint()
-        {
-            int numberOfThreads = 0;
-            while (numberOfThreads != 1 && numberOfThreads != 2)
-            {
-                Console.WriteLine("Do you want to run PrintNumbers in 1 or 2 strings?");
-                int userInput = int.Parse(Console.ReadLine());
-                if (userInput == 1 || userInput == 2) numberOfThreads = userInput;
-            }
 
-            if (numberOfThreads == 1)
-            {
-                PrintNumbers(); // Only use current thread
-            } else
-            {
-                Thread worker = new Thread(PrintNumbers); // Create a new thread and delegate a method to run
-                worker.Name = "Worker"; // Give it a name
-                worker.IsBackground = true; // Make it a background thread.
-                worker.Start(); // Start the worker thread and allow the function to run in a separate thread
-
-                PrintNumbers(); // Run it in the current thread too.
-            }
-        }
 
         public static void PrintNumbers()
         {
+            Random random = new Random();
             // Show thread summary
             PrintThreadInfo(); 
 
             for (int i = 1; i < 11; i++)
             {
                 Console.WriteLine($"{Thread.CurrentThread.Name} - {i}"); // Print thread name and int i
-                Thread.Sleep(1999); // Sleep for approx. 2 seconds.
+                int sleepTime = random.Next(50, 1000);
+                Thread.Sleep(sleepTime); // Sleep for random amount of time between 50 and 1000ms.
             }
         }
 
--- ThreadStats/Program.cs
+++ ThreadStats/Program.cs
@@ -20,13 +20,42 @@ namespace ThreadStats
 
             Printer.PrintThreadInfo(); // Call info function from main thread
 
-            Printer.StartPrint();
+            StartPrint();
 
 
             Console.ReadKey();
 
         }
 
+        public static void StartPrint()
+        {
+            int numberOfThreads = 0;
+            while (numberOfThreads != 1 && numberOfThreads != 2)
+            {
+                Console.WriteLine("Do you want to run PrintNumbers in 1 or if you want 10 enter ''2'' strings?");
+                int userInput = int.Parse(Console.ReadLine());
+                if (userInput == 1 || userInput == 2) numberOfThreads = userInput;
+            }
+
+            if (numberOfThreads == 1)
+            {
+                Printer.PrintNumbers(); // Only use current thread
+            }
+            else
+            {
+                Thread[] threads = new Thread[10];
+                for (int i = 0; i < threads.Length; i++)
+                {
+                    threads[i] = new Thread(Printer.PrintNumbers);
+                    threads[i].Name = $"Worker_{i}"; // Give it an unique name
+                    threads[i].IsBackground = true; // Make it a background thread.
+                    threads[i].Start(); // Start the worker thread and allow the function to run in a separate thread
+                }
+                
+
+                Printer.PrintNumbers(); // Run it in the current thread too.
+            }
+        }
 
     }
 }
