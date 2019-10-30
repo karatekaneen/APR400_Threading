b991651f5e47ebe8af0f12dfc44ea8c0d5f7078b
Karatekaneen <bauhn_@hotmail.com>
2019-10-30 15:33:23 +0100

    Create a new Console Application project named ThreadStats to print out various statistics(Thread Name, has thread started? Priority Level and Thread Stat) which related the currently executing thread(the primary thread of execution).

--- ThreadStats/Program.cs
+++ ThreadStats/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ThreadStats
@@ -10,6 +11,22 @@ namespace ThreadStats
     {
         static void Main(string[] args)
         {
+            /*
+             * This is my lab about threading for the course APR400, Advanced C#. 
+             * Each commit finishes the task mentioned in the commit message.
+             */
+
+            Thread.CurrentThread.Name = "MainThread"; // Set main name
+
+            PrintThreadInfo(); // Call info function from main thread
+
+            Console.ReadKey();
+
+        }
+
+        public static void PrintThreadInfo()
+        {
+            Console.WriteLine($"Name: {Thread.CurrentThread.Name} \n Priority: {Thread.CurrentThread.Priority} \n State: {Thread.CurrentThread.ThreadState} \n Is background thread: {Thread.CurrentThread.IsBackground}");
         }
     }
 }
